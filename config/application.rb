ec38c91b37bb40f27a8c3a38629518bd2401ecb7
youknow <ykc@daycrift.net>
2019-04-16 12:40:28 +0000

    hide test local setting

--- config/application.rb
+++ config/application.rb
@@ -1,6 +1,7 @@
 require_relative 'boot'
 
 require 'rails/all'
+require 'yaml'
 
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
@@ -10,7 +11,11 @@ module HelloApp
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.1
-    config.web_console.whitelisted_ips = '202.209.203.195'
+    conf = YAML.load_file(Rails.root.join(".myconfig"))
+    p conf["myconfig"]["whitelisted_ips"]
+    unless Rails.env.production?
+      config.web_console.whitelisted_ips = conf["myconfig"]["whitelisted_ips"]
+    end
 
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
